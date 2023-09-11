4ec9f622e136c93e727a2bb11b200304283fe08b
amMicheals <mandrew@insight7.io>
2023-09-11 21:17:27 +0100

    added express.json

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from 'body-parser'
+
 import mongoose from "mongoose";
 import routes from "./routes/index"
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017";
+app.use(express.json());
+
 
 mongoose.connect(dbUrl).then(() => console.log("Connected!"));
 
