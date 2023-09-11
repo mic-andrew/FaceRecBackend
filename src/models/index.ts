fce24623b3647145c4f9cbb1df2571e58bd07630
amMicheals <mandrew@insight7.io>
2023-09-11 22:18:01 +0100

    added principal to model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,14 +1,13 @@
 import { model } from "mongoose";
 import { User } from "./userModel";
-import { Admin, Student, Teacher } from "./roleModels";
+import { Admin, Principal, Student, Teacher } from "./roleModels";
 import { Subject } from "./subject";
-
-
 
 export const models = {
   User: model("User", User),
   Student: model("Student", Student),
   Teacher: model("Teacher", Teacher),
-  Admin: model("Topic", Admin),
+  Admin: model("Admin", Admin),
   Subject: model("Subject", Subject),
+  Principal: model("Principal", Principal),
 };
