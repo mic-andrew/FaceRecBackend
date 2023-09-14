7c06fe383891d31b496b913e71acc9ce5aa53094
amMicheals <mandrew@insight7.io>
2023-09-14 03:56:40 +0100

    setup cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import bodyParser from 'body-parser'
+import cors from "cors"
 
 import mongoose from "mongoose";
 import routes from "./routes/index"
@@ -11,6 +11,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017";
+app.use(cors());
+
 app.use(express.json());
 
 export const jwtSecret = process.env.SESSION_SECRET || ""
