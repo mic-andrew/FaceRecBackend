75c70903f57cad013e98e2a3af430f127155474b
amMicheals <mandrew@insight7.io>
2023-09-16 05:54:25 +0100

    passed userData to extended schema

--- src/auth/service.ts
+++ src/auth/service.ts
@@ -4,23 +4,22 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { jwtSecret } from "../app";
 
-const createRole = async (userId: any, role: string) => {
+const createRole = async (userId: any, role: string, userData: any) => {
   switch (role) {
     case "student":
       await models.Student.create({
         user: userId,
-        parentPhoneNumber: "parentPhoneNumber",
-        classRoom: 1,
+        ...userData,
       });
       break;
     case "teacher":
-      await models.Teacher.create({ user: userId });
+      await models.Teacher.create({ user: userId, ...userData });
       break;
     case "principal":
-      await models.Principal.create({ user: userId });
+      await models.Principal.create({ user: userId, ...userData });
       break;
     case "admin":
-      await models.Admin.create({ user: userId });
+      await models.Admin.create({ user: userId, ...userData });
       break;
     default:
       throw new Error("Unknown role");
@@ -34,20 +33,20 @@ export const registerUserService = async (userData: IUser) => {
     });
 
     if (existingUser) {
-      return { error: { message: "Email already exists" } };
+      return { error: true, message: "Email already exists" };
     }
     const encryptedPassword = await bcrypt.hash(userData.password, 10);
     userData.password = encryptedPassword;
-    const newUser = new models.User(userData);
+    const newUser = new models.User(userData);  
     await newUser.save();
 
     const userId = newUser._id;
 
-    createRole(userId, userData.role);
+    await createRole(userId, userData.role, userData);
 
     return {
       success: true,
-      message: "User created successfully",
+      message: `${userData.role} created successfully`,
       data: newUser,
     };
   } catch (error) {
@@ -58,28 +57,35 @@ export const registerUserService = async (userData: IUser) => {
 
 export const loginService = async (email: string, password: string) => {
   try {
-    const userDetails = { email, password };
+    const userDetails = { email };
     const existingUser: any = await models.User.findOne(userDetails);
-    const decodedPassword = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
+
+    console.log(existingUser);
 
     if (existingUser) {
-      const token = await jwt.sign(existingUser, jwtSecret, {
-        expiresIn: "1h",
-      });
-      existingUser.token = token;
+      const decodedPassword = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
 
-      return {
-        success: true,
-        message: `Welcome back ${existingUser.firstName}`,
-        data: existingUser,
-      };
+      if (decodedPassword) {
+        console.log("decodedPassword ", decodedPassword);
+        const userId = existingUser._id;
+
+        const token = await jwt.sign({ userId }, jwtSecret, {
+          expiresIn: "12h",
+        });
+        existingUser.token = token;
+        return {
+          success: true,
+          message: `Welcome back ${existingUser.firstName}`,
+          data: existingUser,
+        };
+      }
     } else {
       return { error: true, message: "Invalid Email or Password" };
     }
   } catch (error) {
-    return { error: true, message: "something went wrong" };
+    return { error: true, message: error };
   }
 };
