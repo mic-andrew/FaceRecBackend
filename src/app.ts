d3e2353e77265e87695392cfdd9bd64c557b8037
amMicheals <mandrew@insight7.io>
2023-09-16 19:37:50 +0100

    added express static

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import cors from "cors"
-
+import cors from "cors";
 import mongoose from "mongoose";
-import routes from "./routes/index"
-
+import routes from "./routes/index";
 
 dotenv.config();
 
@@ -12,17 +10,16 @@ const app: Express = express();
 const port = process.env.PORT;
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/school";
 app.use(cors());
-
+app.use(express.static('uploads'));
 app.use(express.json());
 
-export const jwtSecret = process.env.SESSION_SECRET || ""
 
 
+export const jwtSecret = process.env.SESSION_SECRET || "";
 
 mongoose.connect(dbUrl).then(() => console.log("Connected!"));
 
-
-app.use("/", routes)
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
