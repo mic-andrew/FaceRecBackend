09427eddad9af2c67631bc6da383545a6d91897c
amMicheals <mandrew@insight7.io>
2023-09-16 06:32:27 +0100

    added portal routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import authRoutes from "../auth/routes"
+import portalRoutes from "../portal/routes"
+
 const router = Router();
 
 
 router.use("/auth", authRoutes);
+router.use("/portal", portalRoutes);
+
 
 
 export default router;
