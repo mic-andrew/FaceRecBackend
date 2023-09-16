8956068e05aa772d742163782fc6bbac25e9a72b
amMicheals <mandrew@insight7.io>
2023-09-16 19:36:47 +0100

    fixed fetch teacher bug

--- src/portal/service.ts
+++ src/portal/service.ts
@@ -2,7 +2,7 @@ import { models } from "../models";
 
 export const viewTeachersOrStudentsService = async (role: string) => {
   const findAll =
-    role === "Teacher"
+    role === "teacher"
       ? await models.Teacher.find()
       : await models.Student.find();
   return findAll;
